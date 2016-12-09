61094f6c91160f8907249fd16226a52242e2a106
Sukanta Maikap <smaikap@mobileiron.com>
2016-12-09 09:37:10 +0530

    find old words from Shakespeare sonnets

---
+++ sonnets/sonnets.py
@@ -0,0 +1,20 @@
+import time
+
+# find the word from Shakespeare sonnet that are not present in modern dictionary
+sonnet_words = [line.strip() for line in open("sonnet_words.txt", "r").readlines()]
+modern_words = [line.strip() for line in open("sowpods.txt", "r").readlines()]
+modern_words_dictionary = dict((word, 1) for word in modern_words)
+
+count = 0
+
+start = time.time()
+for word in sonnet_words:
+    # time comparison code
+    #if word not in modern_words
+    if word not in modern_words_dictionary:
+        print(word)
+        count += 1
+stop = time.time()
+
+print("Total not found word count : %d", count)
+print("Time elapsed : %.1f seconds" %(stop - start))
